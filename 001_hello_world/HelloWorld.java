15b833f98652e7be3418b1949e51d5214746246e
ronnieqt <xi.ronnie.chen@outlook.com>
2024-09-29 19:37:11 +0100

    added hello world script

---
+++ 001_hello_world/HelloWorld.java
@@ -0,0 +1,9 @@
+// A simple program the prints text on the terminal
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
+
